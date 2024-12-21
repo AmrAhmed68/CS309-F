1a22cadee224e29d9e3778bb89dfa0d537f74579
AmrAhmed68 <amrahmed682003@gmail.com>
2024-12-22 01:16:38 +0200

    update App.js index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ProductList from "./components/ProductList/ProductList";
+import Navbar from "./components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Categories from "./components/Categories/Categories";
+import Details from "./components/DetailsPage/Details";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import SignUP from "./components/SignUP/SignUP";
+import Profile from "./components/User/Profile";
+import Cart from "./components/Cart/Cart";
+import Favourite from "./components/Favourite/Favourite";
+import FilterProduct from "./components/FilterProduct/FilterProduct";
+import CategoryProduct from "./components/FilterProduct/CategoryProduct";
+import AdminDasboard from "./components/Admin/AdminDasboard";
+import AdminRoute from "./isAdmin";
+import Footer from "./components/Footer/Footer";
+import ContactUs from "./components/ContactUs/ContactUs";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<ProductList />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/admin" element={<AdminRoute><AdminDasboard/></AdminRoute>}/>
+        <Route path="/favourite" element={<Favourite />} />
+        <Route path="category/:categoryName/:subcategoryName" element={<FilterProduct />} />
+        <Route path="category/:categoryName" element={<CategoryProduct />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUP />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/category/:id" element={<Categories />} />
+        <Route path="/details/:id" element={<Details />} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { CartProvider } from './hooks/CartProvider'; // Import the provider
+import { AuthProvider } from './hooks/useAuth';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AuthProvider>
+      <CartProvider>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
+      </CartProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
 
