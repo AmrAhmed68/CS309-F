cd137dcfd3eb641459c849861cfa32db566a8205
AmrAhmed68 <amrahmed682003@gmail.com>
2024-12-22 01:11:23 +0200

    .env Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,53 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
-
+const session = require("express-session");
+const path = require("path");
+const passport = require("passport");
+const bodyParser = require("body-parser");
 const cors = require("cors");
-
-require('dotenv').config();
-
+require("dotenv").config();
 const app = express();
+const fs = require('fs');
 
-const port = process.env.PORT
-
-console.log(port)
-
-const Port = 8000
+app.use(express.urlencoded({ extended: true })); 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(express.json());
 
 app.use(
   cors({
-    origin: "*",
+    origin: "*", 
     credentials: true,
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "your_secret_key",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+const aRoutes = require("./routes/auth.routes");
+app.use("/api", aRoutes);
+
+require("./config/passport")(passport);
+
 mongoose
-  .connect(process.env.MONGO, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -34,3 +57,5 @@ mongoose
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
+
+module.exports = app;
